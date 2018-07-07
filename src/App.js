245b2a7e36cc9b60fae24862afbf9fd49cf46886
shirintag <taghavi.shirin@gmail.com>
2018-07-07 20:04:09 +0200

    Add data fetching for countdown

--- src/App.js
+++ src/App.js
@@ -7,13 +7,51 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            concertDateTime : new Date(1531598400000)
+            concertDateTime: null,
+            loadingData: true,
+            noData: false
         }
+
+        this._fetchData = this._fetchData.bind(this);
     }
+
+    _fetchData() {
+        fetch('./data/concertData.json').then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return response;
+            } else {
+                var error = new Error(response.statusText)
+                error.response = response;
+                throw error;
+            }
+        }).then(
+            response => response.json()
+        ).then(json => {
+            console.log(json);
+            this.setState({
+                concertDateTime: new Date(json.concertTimestamp),
+                loadingData: false
+            })
+        }).catch(error => {
+            console.log(error.response ? error.response : error);
+            this.setState({
+                loadingData: false,
+                noData: true
+            })
+
+        });
+    }
+
+    componentDidMount() {
+        this._fetchData();
+    }
+
     render() {
         return (
             <div className="App">
-                <Countdown concertDateTime={this.state.concertDateTime}/>
+                { this.state.loadingData && <div>Searching for concerts</div> }
+                { this.state.concertDateTime && <Countdown concertDateTime={this.state.concertDateTime}/> }
+                { this.state.noData && <p className="ErrorMessage f-errorMessage">No available concerts.</p> }
             </div>
         );
     }
